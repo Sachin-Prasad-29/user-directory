c8a2b089e21ba1d21a3e6f57238a106e2ca8f34a
Sachin Prasad <sachin.prasad@xlscout.com>
2023-11-29 14:17:13 +0530

    Removed unwanted code and added basic structure of the code

---
+++ src/pages/Home.js
@@ -0,0 +1,13 @@
+// Home.js
+import React from 'react';
+import users from '../assets/user.json'
+
+const Home = () => {
+  return (
+    <div>
+      <h1>Home Page</h1>
+    </div>
+  );
+};
+
+export default Home;
---
+++ src/pages/Post.js
@@ -0,0 +1,13 @@
+// Post.js
+import React from 'react';
+
+const Post = () => {
+  return (
+    <div>
+      <h1>Post Page</h1>
+      {/* Your Post page content */}
+    </div>
+  );
+};
+
+export default Post;
